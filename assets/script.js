0b1382795263622b187831580c901279cfba0234
kyrakun <rizkifirman1933@gmail.com>
2022-09-10 11:09:16 +0700

    updt

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,15 @@ const nav = document.querySelector("nav ul");
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+
+// !SCROLL NAVBAR
+const navbar = document.getElementsByTagName("nav")[0];
+window.addEventListener("scroll", function () {
+  console.log(this.window.scrollY);
+  if (this.window.scrollY > 1) {
+    navbar.classList.replace("bg-transparent", "nav-color");
+  } else if (this.window.scrollY <= 0) {
+    navbar.classList.replace("nav-color", "bg-transparent");
+  }
+});
+// !OWL CAROUSEL
